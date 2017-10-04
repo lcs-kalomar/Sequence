7da4fe9da771e35cfb4153bfbbf1f903fc98ebc4
Khalid Alomar <kalomar@lcs-mba3261.lcs.on.ca>
2017-10-03 21:13:38 -0400

    i did the lines

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -31,15 +31,28 @@ canvas.drawRectangle(centreX: 0, centreY: 0, width: 1000, height: 1200)
 canvas.fillColor = Color.yellow
 canvas.drawEllipse(centreX: 0, centreY: 500, width: 200, height: 200)
 
+// Draw Cloud
+canvas.fillColor = Color .white
+canvas.drawEllipse(centreX: 450, centreY: 500, width: 150, height: 150)
+
+canvas.drawEllipse(centreX: 500, centreY: 450, width: 150, height: 150)
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
 
-
-
-
-
-
-
+//Draw sun rays
+canvas.lineColor = Color.yellow
+canvas.defaultLineWidth=5
+canvas.drawLine(fromX: 0, fromY: 500, toX: 0, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 100, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 200, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 300, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 400, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 100)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 200)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 300)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 400)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 500)
 
 
 
